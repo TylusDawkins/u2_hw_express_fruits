f05fb591327ecf56a79557b5500ca56f23293b6c
ahonore42 <a1honore80@gmail.com>
2021-02-20 01:20:37 -0600

    add fruits to fruits.js

--- fruits.js
+++ fruits.js
@@ -46,6 +46,74 @@ const fruits = [
   {
       name: 'Buddha\'s Hand',
       imgUrl: 'https://i.imgur.com/c892wgq.jpg'
+  },
+  {
+      name: 'Strawberry',
+      imgUrl: 'https://www.searchpng.com/wp-content/uploads/2018/12/Strawberry-png-image.png'
+  },
+  {
+      name: 'Banana',
+      imgUrl: 'https://cdn.shopify.com/s/files/1/0716/4043/products/Banana_grande.jpg?v=1439790120'
+  },
+  {
+      name: 'Mango',
+      imgUrl: 'https://momobud.sg/wp-content/uploads/2017/03/Keitt-mango-3.jpg'
+  },
+  {
+      name: 'Pineapple',
+      imgUrl: 'https://antioxidant-fruits.com/wp-content/uploads/2015/10/Fresh-Golden-Pineapple.jpg'
+  },
+  {
+      name: 'Orange',
+      imgUrl: 'https://4.bp.blogspot.com/--9TJIhhMcyw/TiqHMTWwdqI/AAAAAAAADAk/vgMrXPRP0dw/s1600/orange_798.jpg'
+  },
+  {
+      name: 'Peach',
+      imgUrl: 'https://flavourstogo.ca/wp-content/uploads/2018/01/Juicy-Peach-Food-Flavour-by-Capella-1.jpg'
+  },
+  {
+      name: 'Red Grape',
+      imgUrl: 'https://royalfruitz.com/wp-content/uploads/2018/02/red-grape.jpg'
+  },
+  {
+      name: 'Watermelon',
+      imgUrl: 'https://www.parasperfumers.com/upload/product_ecom/Watermelon-Hydrosol-Citrullus-vulgaris-Hydrolat-ProductPic.jpg'
+  },
+  {
+      name: 'Cantaloupe',
+      imgUrl: 'https://i5.walmartimages.com/asr/fb4c18a5-9367-4770-b99f-7518c72db482.5609c32e87a3110b734aad048bf9fe35.jpeg'
+  },
+  {
+      name: 'Avocado',
+      imgUrl: 'https://blogs.kcrw.com/goodfood/wp-content/uploads/2013/04/Avocado.jpg'
+  },
+  {
+      name: 'Cherry',
+      imgUrl: 'https://www.heijmul.nl/wp-content/uploads/2016/12/hookah-tobacco-cherry.png'
+  },
+  {
+      name: 'Green Pear',
+      imgUrl: 'https://www.trimsfresh.com.au/wp-content/uploads/2020/03/Green-pear.jpg'
+  },
+  {
+      name: 'Blueberry',
+      imgUrl: 'https://cdn.shopify.com/s/files/1/2971/2126/products/Blueberry_2000x.jpg'
+  },
+  {
+      name: 'Acai Berry',
+      imgUrl: 'https://cdn.shopify.com/s/files/1/1613/2065/articles/Acai_Berry_Benefits.jpg'
+  },
+  {
+      name: 'Rasberry',
+      imgUrl: 'https://cdn.shopify.com/s/files/1/2971/2126/products/Raspberry_ed3f1df5-030e-4999-ab05-732f0c679196_2000x.jpg'
+  },
+  {
+      name: 'Blackberry',
+      imgUrl: 'https://fruitlayer.com/media/k2/items/cache/d61d44254608dd06ccdd2ff02982d14d_XL.jpg'
+  },
+  {
+      name: 'Coconut',
+      imgUrl: 'https://5.imimg.com/data5/OH/IC/MY-4090147/mature-coconut-meat-500x500.jpg'
   }
 ]
 
